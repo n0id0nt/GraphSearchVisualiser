1b666ed6b63162947062a85ce181f3fb65950ce5
Herbert <102568830@student.swin.edu.au>
2020-05-14 13:05:42 +1000

    added batch file

---
+++ test.bat
@@ -0,0 +1,8 @@
+set file="EnviromentGrids/RobotNav1.txt"
+bin\Release\netcoreapp2.1\win10-x64\Search %file% DFS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% BFS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% GBFS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% AS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% IDDFS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% RBFS
+bin\Release\netcoreapp2.1\win10-x64\Search %file% GUI
